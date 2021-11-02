55560c365f46c0061ac3598ae01182e7daeb461a
Manuel Ayrton Caamaño <caamanomanuel@gmail.com>
2021-11-02 00:32:14 -0300

    Desafio 3 JS

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+function NavBar() {
+
+    return (
+
+
+        <div className="headerContainer">
+            <nav className="appHeader">
+                <div href="#" className="brand">ALMOST</div>
+                <ul className="Links">
+                    <li href="#">PRODUCTOS</li>
+                    <li href="#">SOBRE NOSOTROS</li>
+                    <li href="#">CONTACTO</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export {NavBar};
